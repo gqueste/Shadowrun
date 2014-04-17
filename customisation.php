0de97156b0b8a073d226bb12880054e244581b27
gqueste <gabriel.queste@gmail.com>
2014-04-17 23:12:50 +0200

    Fin préparation case typique tableau caractéristiques Ajout de la caractéristique magie

--- customisation.php
+++ customisation.php
@@ -41,11 +41,12 @@
 <?php
 
 $totaux_points = 400;
+$points_competences = $totaux_points / 2;
 $points_disponibles = 0;
 $points_attributs = array(
-  'actuels' => array('constitution' => 0, 'agilite' => 0, 'reaction' => 0, 'force' => 0, 'charisme' => 0, 'intelligence' => 0, 'logique' => 0, 'volonte' => 0, 'chance' => 0, 'initiative' => 0),
-  'minimums' => array('constitution' => 0, 'agilite' => 0, 'reaction' => 0, 'force' => 0, 'charisme' => 0, 'intelligence' => 0, 'logique' => 0, 'volonte' => 0, 'chance' => 0, 'initiative' => 0),
-  'maximums' => array('constitution' => 0, 'agilite' => 0, 'reaction' => 0, 'force' => 0, 'charisme' => 0, 'intelligence' => 0, 'logique' => 0, 'volonte' => 0, 'chance' => 0, 'initiative' => 0));
+  'actuels' => array('constitution' => 0, 'agilite' => 0, 'reaction' => 0, 'force' => 0, 'charisme' => 0, 'intelligence' => 0, 'logique' => 0, 'volonte' => 0, 'chance' => 0, 'initiative' => 0, 'magie' => 0),
+  'minimums' => array('constitution' => 0, 'agilite' => 0, 'reaction' => 0, 'force' => 0, 'charisme' => 0, 'intelligence' => 0, 'logique' => 0, 'volonte' => 0, 'chance' => 0, 'initiative' => 0, 'magie' => 0),
+  'maximums' => array('constitution' => 0, 'agilite' => 0, 'reaction' => 0, 'force' => 0, 'charisme' => 0, 'intelligence' => 0, 'logique' => 0, 'volonte' => 0, 'chance' => 0, 'initiative' => 0, 'magie' => 0));
 
 init();
 
@@ -62,41 +63,41 @@ function init() {
     case 'Ork':
       $points_disponibles = $totaux_points - 20;
       $points_attributs = array(
-        'actuels' => array('constitution' => 4, 'agilite' => 1, 'reaction' => 1, 'force' => 3, 'charisme' => 1, 'intelligence' => 1, 'logique' => 1, 'volonte' => 1, 'chance' => 1, 'initiative' => 0),
-        'minimums' => array('constitution' => 4, 'agilite' => 1, 'reaction' => 1, 'force' => 3, 'charisme' => 1, 'intelligence' => 1, 'logique' => 1, 'volonte' => 1, 'chance' => 1, 'initiative' => 0),
-        'maximums' => array('constitution' => 9, 'agilite' => 6, 'reaction' => 6, 'force' => 8, 'charisme' => 5, 'intelligence' => 6, 'logique' => 5, 'volonte' => 6, 'chance' => 10, 'initiative' => 0));
+        'actuels' => array('constitution' => 4, 'agilite' => 1, 'reaction' => 1, 'force' => 3, 'charisme' => 1, 'intelligence' => 1, 'logique' => 1, 'volonte' => 1, 'chance' => 1, 'initiative' => 0, 'magie' => 0),
+        'minimums' => array('constitution' => 4, 'agilite' => 1, 'reaction' => 1, 'force' => 3, 'charisme' => 1, 'intelligence' => 1, 'logique' => 1, 'volonte' => 1, 'chance' => 1, 'initiative' => 0, 'magie' => 0),
+        'maximums' => array('constitution' => 9, 'agilite' => 6, 'reaction' => 6, 'force' => 8, 'charisme' => 5, 'intelligence' => 6, 'logique' => 5, 'volonte' => 6, 'chance' => 10, 'initiative' => 0, 'magie' => 0));
       break;
 
     case 'Nain':
       $points_disponibles = $totaux_points - 25;
       $points_attributs = array(
-        'actuels' => array('constitution' => 2, 'agilite' => 1, 'reaction' => 1, 'force' => 3, 'charisme' => 1, 'intelligence' => 1, 'logique' => 1, 'volonte' => 2, 'chance' => 1, 'initiative' => 0),
-        'minimums' => array('constitution' => 2, 'agilite' => 1, 'reaction' => 1, 'force' => 3, 'charisme' => 1, 'intelligence' => 1, 'logique' => 1, 'volonte' => 2, 'chance' => 1, 'initiative' => 0),
-        'maximums' => array('constitution' => 7, 'agilite' => 6, 'reaction' => 5, 'force' => 8, 'charisme' => 6, 'intelligence' => 6, 'logique' => 6, 'volonte' => 7, 'chance' => 10, 'initiative' => 0));
+        'actuels' => array('constitution' => 2, 'agilite' => 1, 'reaction' => 1, 'force' => 3, 'charisme' => 1, 'intelligence' => 1, 'logique' => 1, 'volonte' => 2, 'chance' => 1, 'initiative' => 0, 'magie' => 0),
+        'minimums' => array('constitution' => 2, 'agilite' => 1, 'reaction' => 1, 'force' => 3, 'charisme' => 1, 'intelligence' => 1, 'logique' => 1, 'volonte' => 2, 'chance' => 1, 'initiative' => 0, 'magie' => 0),
+        'maximums' => array('constitution' => 7, 'agilite' => 6, 'reaction' => 5, 'force' => 8, 'charisme' => 6, 'intelligence' => 6, 'logique' => 6, 'volonte' => 7, 'chance' => 10, 'initiative' => 0, 'magie' => 0));
       break;
 
     case 'Elfe':
       $points_disponibles = $totaux_points - 30;
       $points_attributs = array(
-        'actuels' => array('constitution' => 1, 'agilite' => 2, 'reaction' => 1, 'force' => 1, 'charisme' => 3, 'intelligence' => 1, 'logique' => 1, 'volonte' => 1, 'chance' => 1, 'initiative' => 0),
-        'minimums' => array('constitution' => 1, 'agilite' => 2, 'reaction' => 1, 'force' => 1, 'charisme' => 3, 'intelligence' => 1, 'logique' => 1, 'volonte' => 1, 'chance' => 1, 'initiative' => 0),
-        'maximums' => array('constitution' => 6, 'agilite' => 7, 'reaction' => 6, 'force' => 6, 'charisme' => 8, 'intelligence' => 6, 'logique' => 6, 'volonte' => 6, 'chance' => 10, 'initiative' => 0));
+        'actuels' => array('constitution' => 1, 'agilite' => 2, 'reaction' => 1, 'force' => 1, 'charisme' => 3, 'intelligence' => 1, 'logique' => 1, 'volonte' => 1, 'chance' => 1, 'initiative' => 0, 'magie' => 0),
+        'minimums' => array('constitution' => 1, 'agilite' => 2, 'reaction' => 1, 'force' => 1, 'charisme' => 3, 'intelligence' => 1, 'logique' => 1, 'volonte' => 1, 'chance' => 1, 'initiative' => 0, 'magie' => 0),
+        'maximums' => array('constitution' => 6, 'agilite' => 7, 'reaction' => 6, 'force' => 6, 'charisme' => 8, 'intelligence' => 6, 'logique' => 6, 'volonte' => 6, 'chance' => 10, 'initiative' => 0, 'magie' => 0));
       break;
 
     case 'Troll':
       $points_disponibles = $totaux_points - 40;
       $points_attributs = array(
-        'actuels' => array('constitution' => 5, 'agilite' => 1, 'reaction' => 1, 'force' => 5, 'charisme' => 1, 'intelligence' => 1, 'logique' => 1, 'volonte' => 1, 'chance' => 1, 'initiative' => 0),
-        'minimums' => array('constitution' => 5, 'agilite' => 1, 'reaction' => 1, 'force' => 5, 'charisme' => 1, 'intelligence' => 1, 'logique' => 1, 'volonte' => 1, 'chance' => 1, 'initiative' => 0),
-        'maximums' => array('constitution' => 10, 'agilite' => 5, 'reaction' => 6, 'force' => 10, 'charisme' => 4, 'intelligence' => 5, 'logique' => 5, 'volonte' => 6, 'chance' => 10, 'initiative' => 0));
+        'actuels' => array('constitution' => 5, 'agilite' => 1, 'reaction' => 1, 'force' => 5, 'charisme' => 1, 'intelligence' => 1, 'logique' => 1, 'volonte' => 1, 'chance' => 1, 'initiative' => 0, 'magie' => 0),
+        'minimums' => array('constitution' => 5, 'agilite' => 1, 'reaction' => 1, 'force' => 5, 'charisme' => 1, 'intelligence' => 1, 'logique' => 1, 'volonte' => 1, 'chance' => 1, 'initiative' => 0, 'magie' => 0),
+        'maximums' => array('constitution' => 10, 'agilite' => 5, 'reaction' => 6, 'force' => 10, 'charisme' => 4, 'intelligence' => 5, 'logique' => 5, 'volonte' => 6, 'chance' => 10, 'initiative' => 0, 'magie' => 0));
       break;
     
     default:
       $points_disponibles = $totaux_points;
       $points_attributs = array(
-        'actuels' => array('constitution' => 1, 'agilite' => 1, 'reaction' => 1, 'force' => 1, 'charisme' => 1, 'intelligence' => 1, 'logique' => 1, 'volonte' => 1, 'chance' => 2, 'initiative' => 0),
-        'minimums' => array('constitution' => 1, 'agilite' => 1, 'reaction' => 1, 'force' => 1, 'charisme' => 1, 'intelligence' => 1, 'logique' => 1, 'volonte' => 1, 'chance' => 2, 'initiative' => 0),
-        'maximums' => array('constitution' => 6, 'agilite' => 6, 'reaction' => 6, 'force' => 6, 'charisme' => 6, 'intelligence' => 6, 'logique' => 6, 'volonte' => 6, 'chance' => 10, 'initiative' => 0));
+        'actuels' => array('constitution' => 1, 'agilite' => 1, 'reaction' => 1, 'force' => 1, 'charisme' => 1, 'intelligence' => 1, 'logique' => 1, 'volonte' => 1, 'chance' => 2, 'initiative' => 0, 'magie' => 0),
+        'minimums' => array('constitution' => 1, 'agilite' => 1, 'reaction' => 1, 'force' => 1, 'charisme' => 1, 'intelligence' => 1, 'logique' => 1, 'volonte' => 1, 'chance' => 2, 'initiative' => 0, 'magie' => 0),
+        'maximums' => array('constitution' => 6, 'agilite' => 6, 'reaction' => 6, 'force' => 6, 'charisme' => 6, 'intelligence' => 6, 'logique' => 6, 'volonte' => 6, 'chance' => 10, 'initiative' => 0, 'magie' => 0));
       break;
   }
 }
@@ -104,19 +105,25 @@ function init() {
 ?>
 
   <div id='caracteristiques'>
-    <h3>Caractéristiques</h3>
-    <table class="table table-bordered" style='text-align:center;'>
+    <h3 id='titre-caracteristiques'>Caractéristiques : XXX / YYY Points disponibles</h3>
+    <table class='table table-bordered' style='text-align:center;'>
       <tr><th style='text-align:center;'>Constitution</th><th style='text-align:center;'>Agilité</th><th style='text-align:center;'>Réaction</th><th style='text-align:center;'>Force</th><th style='text-align:center;'>Charisme</th><th style='text-align:center;'>Intuition</th><th style='text-align:center;'>Logique</th><th style='text-align:center;'>Volonté</th></tr>
       <tr>
-        <td style='width:210px'>
-          <div class="input-group" style='width:200px'>
-            <input class="form-control" id='edit-Constitution' type='number' min='' max ='' value='5' readonly size='2' style='text-align:center;'>
-            <div class="input-group-btn">
-              <span class="input-group-btn">
-                <button class="btn btn-default" type="button">Go!</button>
+        <td style='width:160px'>
+          <div class='input-group' style='width:150px'>
+            <input class='form-control' id='edit-constitution' type='number' min='' max ='' value='' readonly size='2' style='text-align:center;'>
+            <input id='minimum-constitution' type='hidden' value=''>
+            <input id='maximum-constitution' type='hidden' value=''>
+            <div class='input-group-btn'>
+              <span class='input-group-btn'>
+                <button class='btn btn-default' type='button'>
+                  <span class='glyphicon glyphicon-chevron-up'></span>
+                </button>
               </span>
-              <span class="input-group-btn">
-                <button class="btn btn-default" type="button">Go!</button>
+              <span class='input-group-btn'>
+                <button class='btn btn-default' type='button'>
+                  <span class='glyphicon glyphicon-chevron-down'></span>
+                </button>
               </span>
             </div>
           </div>
